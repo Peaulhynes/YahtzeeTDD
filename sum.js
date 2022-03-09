749214f4ae8875871b845231e024fd41a4369bea
Pauline WARGNY <wargny.pauline@gmail.com>
2022-03-09 17:49:41 +0100

    GREEN - Les des 1 1 1 2 3 doivent retourner 8 pour ThreeOfAKind.

--- sum.js
+++ sum.js
@@ -4,5 +4,24 @@ exports.Count = function Count(des) {
     for (var i = 0; i < des.length; i++) {
         result[des[i] - 1] += des[i];
     }
+
+    return result;
+}
+
+exports.ThreeOfAKind = function ThreeOfAKind(des) {
+    var result = 0;
+    var count = [0, 0, 0, 0, 0, 0];
+
+    for (var i = 0; i < des.length; i++) {
+        count[des[i] - 1] += 1;
+    }
+    for (var i = 0; i < count.length; i++) {
+        if (count[i] >= 3){
+            for (var j = 0; j < des.length; j++) {
+                result += des[j];
+            }
+        }
+    }
+
     return result;
 }
