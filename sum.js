3d1625f0fc5d19b0adc00e3b00d01bbd0b077751
Pauline WARGNY <wargny.pauline@gmail.com>
2022-03-09 17:56:54 +0100

    REFACTO - Les des 1 1 1 2 3 doivent retourner 8 pour ThreeOfAKind.

--- sum.js
+++ sum.js
@@ -1,27 +1,28 @@
+const TestEnum = Object.freeze({
+    Ones:0,
+    Twos:1,
+    Threes:2,
+    Fours:3,
+    Fives:4,
+    Sixes:5,
+    ThreeOfAKind:6
+});
+
 exports.Count = function Count(des) {
-    var result = [0, 0, 0, 0, 0, 0];
+    var total = 0;
+    var count = [0, 0, 0, 0, 0, 0];
+    var result = [0, 0, 0, 0, 0, 0, 0];
 
     for (var i = 0; i < des.length; i++) {
         result[des[i] - 1] += des[i];
-    }
-
-    return result;
-}
-
-exports.ThreeOfAKind = function ThreeOfAKind(des) {
-    var result = 0;
-    var count = [0, 0, 0, 0, 0, 0];
-
-    for (var i = 0; i < des.length; i++) {
         count[des[i] - 1] += 1;
+        total += des[i];
     }
+
     for (var i = 0; i < count.length; i++) {
-        if (count[i] >= 3){
-            for (var j = 0; j < des.length; j++) {
-                result += des[j];
-            }
+        if (count[i] == 3){
+            result[TestEnum.ThreeOfAKind] = total;
         }
     }
-
     return result;
 }
--- sum.test.js
+++ sum.test.js
@@ -1,28 +1,33 @@
 const mod = require("./sum.js");
 
+const TestEnum = Object.freeze({
+    Ones:0,
+    Twos:1,
+    Threes:2,
+    Fours:3,
+    Fives:4,
+    Sixes:5,
+    ThreeOfAKind:6
+});
+
 
 
 test('Les des 1 1 1 1 1 doivent retourner 5 pour Ones', () => {
     var des = [1, 1, 1, 1, 1];
-    expect(mod.Count(des)[0]).toBe(5);
+    expect(mod.Count(des)[TestEnum.Ones]).toBe(5);
 });
 
 test('Les des 1 2 3 4 5 doivent retourner 2 pour Twos.', () => {
     var des = [1, 2, 3, 4, 5];
-    expect(mod.Count(des)[1]).toBe(2);
+    expect(mod.Count(des)[TestEnum.Twos]).toBe(2);
 });
 
 test('Les des 1 6 3 6 5 doivent retourner 12 pour Sixes.', () => {
     var des = [1, 6, 3, 6, 5];
-    expect(mod.Count(des)[5]).toBe(12);
-});
-
-test('Les des 1 6 3 6 5 doivent retourner 12 pour Sixes.', () => {
-    var des = [1, 6, 3, 6, 5];
-    expect(mod.Count(des)[5]).toBe(12);
+    expect(mod.Count(des)[TestEnum.Sixes]).toBe(12);
 });
 
 test('Les des 1 1 1 2 3 doivent retourner 8 pour ThreeOfAKind.', () => {
     var des = [1, 1, 1, 2, 3];
-    expect(mod.ThreeOfAKind(des)).toBe(8);
+    expect(mod.Count(des)[TestEnum.ThreeOfAKind]).toBe(8);
 });
