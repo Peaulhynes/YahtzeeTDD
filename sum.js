3323e0cd4d985d2cf9656abf5361acb733cfed3b
Pauline WARGNY <wargny.pauline@gmail.com>
2022-03-09 17:29:48 +0100

    GREEN - Les des 1 2 3 4 5 doivent retourner 2 pour Twos.

--- sum.js
+++ sum.js
@@ -6,6 +6,16 @@ exports.Ones = function Ones(des) {
             result++;
         }
     }
+    return result;
+}
 
+exports.Twos = function Twos(des) {
+    var result=0;
+
+    for (var i = 0; i < des.length; i++) {
+        if (des[i] == 2) {
+            result+=2;
+        }
+    }
     return result;
 }
