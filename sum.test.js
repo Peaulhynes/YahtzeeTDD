050aa7ddd98c81a8b482857ec30c12186adbff43
Pauline WARGNY <wargny.pauline@gmail.com>
2022-03-09 17:28:00 +0100

    RED - Les des 1 2 3 4 5 doivent retourner 2 pour Twos.

--- sum.test.js
+++ sum.test.js
@@ -4,3 +4,8 @@ test('Les des 1 1 1 1 1 doivent retourner 5 pour Ones', () => {
     var des = [1, 1, 1, 1, 1];
     expect(mod.Ones(des)).toBe(5);
 });
+
+test('Les des 1 2 3 4 5 doivent retourner 2 pour Twos.', () => {
+    var des = [1, 2, 3, 4, 5];
+    expect(mod.Twos(des)).toBe(2);
+});
